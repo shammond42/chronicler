9eddb6606b0fbc49df15ccb19ad59e1c0b2a9994
Steven Hammond <shammond@northpub.com>
2011-01-23 13:18:26 -0500

    Start of publishing a journal.

---
+++ lib/publish.rb
@@ -0,0 +1,11 @@
+module Chronicler
+  module Publish
+    def publish
+      # get authorized user
+      # get campaign
+      # get journals
+    end
+  end
+  
+  include Chronicler::Publish
+end
