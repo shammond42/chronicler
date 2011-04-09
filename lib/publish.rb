12db64eb693aa1a1e50f9f75dc3c013d5482c2a2
Jeffrey Chupp <jeff@semanticart.com>
2011-04-09 13:30:58 -0400

    initial epub generation

--- lib/publish.rb
+++ lib/publish.rb
@@ -1,3 +1,4 @@
+require 'eeepub'
 module Chronicler
   module Publish
     def publish
@@ -15,10 +16,41 @@ module Chronicler
       campaign.players.each do |player|
         puts "#{player.username}: #{player.campaigns.count}"
       end
-      # get campaign
-      
-      
-      # get journals
+
+      File.open("/tmp/title_page.html", "w") do |f|
+        f.print "<center>"
+        f.print "<h1>#{campaign.name}</h1>"
+        f.print "<img src='#{campaign.banner_image_url}' />" unless campaign.banner_image_url.nil?
+        f.print "</center>"
+      end
+
+      html_files = ["/tmp/title_page.html"]
+      nav_sections = []
+
+      campaign.posts.each do |post|
+        html_files << "/tmp/#{post.id}.html"
+        label = "#{post.created_at.to_date} - #{post.post_title}"
+        nav_sections << {:label => label, :content => "#{post.id}.html"}
+        File.open(html_files.last, "w") do |f|
+          f.print "<h1>#{label}</h1>"
+          f.print "<h2>#{post.post_tagline}</h2>" unless post.post_tagline.nil?
+          f.print post.body_html
+        end
+      end
+
+      epub = EeePub.make do
+        title       campaign.name
+        creator     campaign.game_master_id # TODO
+        publisher   campaign.game_master_id # TODO
+        date        Date.today
+        identifier  campaign.campaign_url, :scheme => 'URL'
+        uid         campaign.id
+
+        files html_files
+        nav nav_sections
+      end
+
+      epub.save("campaign.epub")
     end
   end
   
