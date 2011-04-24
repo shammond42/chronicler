ab60873b3c2607057623cc745f802d9c9780f6f9
Steven Hammond <shammond@northpub.com>
2011-04-24 09:42:28 -0400

    Use lower level api. Some progress against, invalid epub files.

--- lib/fate.rb
+++ lib/fate.rb
@@ -11,49 +11,102 @@ module Chronicler
       @dir_name = 'book'
       Dir.mkdir(@dir_name) unless File.exists?(@dir_name)
       File.open("#{@dir_name}/title_page.html", "w") do |f|
-        f.puts "<center>"
+        render_headers(f)
+        f.puts "<div style=\"text-align: center\">"
         f.puts "<h1>#{book['title']}</h1>"
-        f.puts "<img src=\"#{book['cover-image']}\""
+        f.puts "<img src=\"#{book['cover-image']}\" />"
         f.puts "<h4>Authors: #{book['people']['authors'].join(', ')}</h4>"
         f.puts "<h4>Editors: #{book['people']['editors'].join(', ')}</h4>"
         f.puts "<h4>Typesetting: #{book['people']['typesetting'].join(', ')}</h4>"
         f.puts "<h4>ePub Conversion: #{book['people']['epub conversion'].join(', ')}</h4>"
         f.puts "<p>#{book['rights']}</p>"
-        f.puts "</center>"
+        f.puts "</div>"
+        render_footers(f)
       end
 
-      html_files = ["#{@dir_name}/title_page.html"]
+      html_files = ["title_page.html"]
       nav_sections = []
 
       book['chapters'].each do |chapter|
         page = @db.get(chapter)
-        html_files << "book/#{page['_id']}.html"
-        File.open(html_files.last, "w") do |f|
+        html_files << "#{page['_id']}.html"
+        File.open("#{@dir_name}/#{html_files.last}", "w") do |f|
           section_html, section_nav = render_section(page, page['_id'])
-          f.puts(section_html)
+          render_headers(f)
+          f.puts(escape_text(section_html))
+          render_footers(f)
           nav_sections << section_nav if section_nav
         end
       end
       puts ''
 
-      epub = EeePub.make do
-        title       book['title']
-        creator     book['people']['authors'].join(', ')
-        publisher   book['people']['epub conversion']
-        date        Date.today.strftime('%B %d, %Y')
-        identifier  book['identifier'], :scheme => 'URL'
-        uid         book['uid']
-
-        files html_files
-        nav nav_sections
-      end
-
-      epub.save("fate_rpg.epub")
+      generate_epub(book, @dir_name, nav_sections, html_files)
+      # epub = EeePub.make do
+      #   title       book['title']
+      #   creator     book['people']['authors'].join(', ')
+      #   publisher   book['people']['epub conversion']
+      #   date        Date.today.strftime('%B %d, %Y')
+      #   identifier  book['identifier'], :scheme => 'URL'
+      #   uid         book['uid']
+      # 
+      #   files html_files
+      #   nav nav_sections
+      # end
+      # 
+      # epub.save("fate_rpg.epub")
     end
   end
 
+  def generate_epub(book, dir_name, nav_sections, files)
+    EeePub::NCX.new(
+      :uid => book['uid'],
+      :title => book['title'],
+      :nav => nav_sections
+    ).save(File.join(dir_name, 'toc.ncx'))
+
+    EeePub::OPF.new(
+      :title => book['title'],
+      :identifier => book['identifier'],
+      :manifest => files,
+      :ncx => 'toc.ncx'
+    ).save(File.join(dir_name, 'content.opf'))
+
+    EeePub::OCF.new(
+      :dir => dir_name,
+      :container => 'content.opf'
+    ).save('fate_rpg.epub')  
+  end
+  
+  def render_headers(f)
+    f.puts "<?xml version=\"1.0\" encoding=\"windows-1250\"?>"
+    f.puts "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\"
+    \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">"
+    f.puts "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" >"
+    f.puts "<head>"
+    f.puts "</head>"
+    f.puts "<body>"    
+  end
+  
+  def render_footers(f)
+    f.puts "</body>"
+    f.puts "</html>"    
+  end
+  
   def html_id(label)
     label.downcase.gsub(/[\W]+/,'-')
+  end
+  
+  def escape_text(text)
+    new_text = text.dup
+    new_text.gsub!('<colgroup>','')
+    new_text.gsub!('</colgroup>','')
+    new_text.gsub!(/^<col.*/,'')
+    new_text.gsub!('–','&mdash;')
+    new_text.gsub!('‘','&lsquo;')
+    new_text.gsub!('’','&rsquo;')
+    new_text.gsub!('“','&ldquo;')
+    new_text.gsub!('”','&rdquo;')
+    new_text.gsub!(/^[\n\r]/,'') # empty lines
   end
   
   def section_number(section_doc)
