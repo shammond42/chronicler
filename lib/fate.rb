f74130a10f8e81f94bf11e759b97603d01c2d9dc
Steven Hammond <shammond@northpub.com>
2011-04-27 11:01:48 -0400

    Fixed encoding in xml header. This DOES NOT fix encoding issues in the text itself.

--- lib/fate.rb
+++ lib/fate.rb
@@ -82,7 +82,7 @@ module Chronicler
   end
   
   def render_headers(f, chapter)
-    f.puts "<?xml version=\"1.0\" encoding=\"windows-1250\"?>"
+    f.puts "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
     f.puts "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\"
     \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">"
     f.puts "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" >"
